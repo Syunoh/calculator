f0fec69d2d0d7963b4c033db5d71de80995f435d
Syunoh <100738448+Syunoh@users.noreply.github.com>
2022-05-04 10:15:24 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 import setuptools
 
-setuptools.setup(
-  include_package_data = True,
-  name = 'calculator001',
-  version='1.0.0',
-  description='oss_calculator',
-  author='Syunoh',
-  author_email='kan1251@donga.ac.kr',
-  url = 'https://https://github.com/Syunoh/calculator/',
-  download_url = "https://github.com/Syunoh/calculator/archive/refs/tags/v1.0.0.zip"
-  install_requires=['pyteset'],
-  long_description='oss calc',
-  long_description_connet_type = 'text/markdown',
+setuptools.setup;
+  include_package_data = True;
+  name = 'calculator001';
+  version='1.0.0';
+  description='oss_calculator';
+  author='Syunoh';
+  author_email='seoyunoh505@gmail.com';
+  url = 'https://https://github.com/Syunoh/calculator/';
+  download_url = "https://github.com/Syunoh/calculator/archive/refs/tags/v1.0.0.zip";
+  install_requires=['pyteset'];
+  long_description='oss calc';
+  long_description_connet_type = 'text/markdown';
   classifers=[
-    "Programing Language :: Python :: 3",
-    "Operating System :: OS Independent",
-  ],
+    "Programing Language :: Python :: 3";
+    "Operating System :: OS Independent";
+  ];
 )
