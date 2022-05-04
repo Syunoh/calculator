73d3969c415da11675ffa79a9c2cbb238a6cc2d0
Syunoh <100738448+Syunoh@users.noreply.github.com>
2022-05-04 09:23:03 +0900

    Update and rename README.md to setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+  include_package_data = True,
+  name = 'calculator001',
+  version='1.0.0',
+  description='oss_calculator',
+  author='Syunoh',
+  author_email='kan1251@donga.ac.kr',
+  url = 'https://https://github.com/Syunoh/calculator/',
+  install_requires=['pyteset'],
+  long_description='oss calc',
+  long_description_connet_type = 'text/markdown',
+  classifers=[
+    "Programing Language :: Python :: 3",
+    "Operating System :: OS Independent",
+  ],
+)
